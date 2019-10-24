75b1ca959fc9b485659ce8b1409653edcbced85a
antonellinux <56957636+antonellinux@users.noreply.github.com>
2019-10-24 15:00:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 
+
 # ======================================================================
 # IMPORT MODULES
 # ======================================================================
@@ -7,20 +8,40 @@ import pandas as pd
 from __flowers__ import *
 
 # ======================================================================
-# CLASS CREATE TABS
+# CREATE TABS
 # ======================================================================
 
-def create_tab():
+def create_tab_one():
 
-	df = pd.read_csv('data.csv', dtype={'yyyymm':'str', 'yyyymmdd':'str'})
+	##### import data
+
+	df1 = pd.read_csv('data1.csv')
+	df2 = pd.read_csv('data2.csv')
+	df3 = pd.read_csv('data3.csv', dtype={'yyyymmdd':'str'})
+
+	##### define last day
+
+	s_max_day = df3['yyyymmdd'].max()
+
+	##### create objects
+
+	params = {'height':400, 'width':800, 'barwidth':0.7, 'title':'Region weight (data @: ' + s_max_day + ')', 'default':'OVERALL'}
+	o_plot1, o_drop1 = BarPlotWithDropDown(df1, 'region', 'weight', 'portfolio_name', params, False).create_widgets()
+
+	params = {'height':400, 'width':800, 'barwidth':0.7, 'title':'Sector weight (data @: ' + s_max_day + ')', 'default':'OVERALL'}
+	o_plot2, o_drop2 = BarPlotWithDropDown(df2, 'sector_name', 'weight', 'portfolio_name', params, False).create_widgets()
+
+	params = {'height':840, 'width':800, 'barwidth':0.7, 'title':'Portolios weight by region', 'default':s_max_day}
+	o_plot3, o_drop3 = MultiBarPlotWithDropDown(df3, ['region', 'portfolio_name'], 'weight', 'yyyymmdd', params, False).create_widgets()
 	
-	o_tbl, o_chk = TableWithDropDown(df, 'yyyymm', {'height':400, 'width':800, 'default':'201910'}).create_widgets()	
-	o_layout = column(o_chk, o_tbl)
-	o_tab = Panel(child = o_layout, title = 'Staging Summary')		
+	o_layout = row(column(column(o_drop1, o_plot1), column(o_drop2, o_plot2)), column(o_drop3, o_plot3)) 
 
-	return o_tab
+	return Panel(child = o_layout, title = 'Segments')		
 
+# ======================================================================
+# WRAP UP
+# ======================================================================
 
-o_tab = create_tab()
+o_tab = create_tab_one()
 o_tabs = Tabs(tabs = [o_tab])
 curdoc().add_root(o_tabs)
