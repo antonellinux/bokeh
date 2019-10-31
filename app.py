593b7e8799aed688ac49b2c7b4a5e4420ba8a253
antonellinux <56957636+antonellinux@users.noreply.github.com>
2019-10-31 14:11:40 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -12,23 +12,17 @@ from __flowers__ import *
 
 o_div_logo = Div(text="<p><img src = 'https://www.localpensionspartnership.org.uk/Files/Templates/Designs/BasicDesign/images/LPP_logo.svg' height = 40 width = 120></p>")
 
-df1 = pd.read_csv('data/data1.csv')
-df2 = pd.read_csv('data/data2.csv')
-df3 = pd.read_csv('data/data3.csv', dtype={'yyyymmdd':'str'})
-df4 = pd.read_csv('data/data4.csv', dtype={'yyyymmdd':'str'})	
-df4_sub = df4[df4['to_ccy'].isin(['EUR', 'USD', 'AUD', 'NZD', 'CHF', 'CAD'])]
-df5 = pd.read_csv('data/data5.csv')	
-df6 = pd.read_csv('data/data6.csv', dtype={'yyyymmdd':'str'})	
-df7 = pd.read_csv('data/data7.csv', dtype={'yyyymmdd':'str'})
-
-s_max_day = df3['yyyymmdd'].max()
-
 # ======================================================================
 # CREATE TABS
 # ======================================================================
 
 def create_tab_one():		
 	
+	df1 = pd.read_csv('data/data1.csv')
+	df2 = pd.read_csv('data/data2.csv')
+	df3 = pd.read_csv('data/data3.csv', dtype={'yyyymmdd':'str'})
+	s_max_day = df3['yyyymmdd'].max()
+
 	params = {'height':370, 'width':800, 'barwidth':0.7, 'title':'Region weight (data @: ' + s_max_day + ')', 'default':'OVERALL'}
 	o_plot1, o_drop1 = BarPlotWithDropDown(df1, 'region', 'weight', 'portfolio_name', params, False).create_widgets()
 
@@ -45,6 +39,9 @@ def create_tab_one():
 
 def create_tab_two():	
 
+	df4 = pd.read_csv('data/data4.csv', dtype={'yyyymmdd':'str'})	
+	df4_sub = df4[df4['to_ccy'].isin(['EUR', 'USD', 'AUD', 'NZD', 'CHF', 'CAD'])]
+
 	o_plot1, o_dd1 = TimeSeriesPlotWithDropDown(df4, 'yyyymmdd', 'fx_rate', 'to_ccy', {'height':400, 'width':800, 'title':'FxRate vs GBP', 'default':'USD'}, True).create_widgets()	
 	o_plot2 = TimeSeriesPlot(df4_sub, 'yyyymmdd', 'fx_rate', 'to_ccy', {'height':440, 'width':800, 'title':'FxRate (AUD-CAD-CHF-EUR-NZD-USD)'}, True).create_plot()
 
@@ -54,6 +51,11 @@ def create_tab_two():
 
 
 def create_tab_three():	
+
+	df5 = pd.read_csv('data/data5.csv')	
+	df6 = pd.read_csv('data/data6.csv', dtype={'yyyymmdd':'str'})	
+	df7 = pd.read_csv('data/data7.csv', dtype={'yyyymmdd':'str'})
+	s_max_day = '20191030'
 	
 	df5.sort_values(by=['portfolio_name', 'mkt_val_gbp'], ascending=[True, False], inplace=True)	
 	o_tbl1, o_chk1 = TableWithDropDown(df5, 'portfolio_name', {'height':280, 'width':800, 'default':'OVERALL'}).create_widgets()	
@@ -74,21 +76,21 @@ def create_tab_three():
 	return Panel(child = o_layout, title = 'Stocks')
 
 
-def create_tab_four():
+# def create_tab_four():
 
-	o_div = Div(text="<b>Download each data source</b>", style={'font-size': '100%'})
+# 	o_div = Div(text="<b>Download each data source</b>", style={'font-size': '100%'})
 
-	o_btn1 = ExportDataButton(df1, 'data1', 'data1.csv').create_button()
-	o_btn2 = ExportDataButton(df2, 'data2', 'data2.csv').create_button()
-	o_btn3 = ExportDataButton(df3, 'data3', 'data3.csv').create_button()
-	o_btn4 = ExportDataButton(df4, 'data4', 'data4.csv').create_button()
-	o_btn5 = ExportDataButton(df5, 'data5', 'data5.csv').create_button()
-	o_btn6 = ExportDataButton(df6, 'data6', 'data6.csv').create_button()
-	o_btn7 = ExportDataButton(df7, 'data7', 'data7.csv').create_button()		
+# 	o_btn1 = ExportDataButton(df1, 'data1', 'data1.csv').create_button()
+# 	o_btn2 = ExportDataButton(df2, 'data2', 'data2.csv').create_button()
+# 	o_btn3 = ExportDataButton(df3, 'data3', 'data3.csv').create_button()
+# 	o_btn4 = ExportDataButton(df4, 'data4', 'data4.csv').create_button()
+# 	o_btn5 = ExportDataButton(df5, 'data5', 'data5.csv').create_button()
+# 	o_btn6 = ExportDataButton(df6, 'data6', 'data6.csv').create_button()
+# 	o_btn7 = ExportDataButton(df7, 'data7', 'data7.csv').create_button()		
 
-	o_layout = column(o_div_logo, o_div, o_btn1, o_btn2, o_btn3, o_btn4, o_btn5, o_btn6, o_btn7)
+# 	o_layout = column(o_div_logo, o_div, o_btn1, o_btn2, o_btn3, o_btn4, o_btn5, o_btn6, o_btn7)
 
-	return Panel(child = o_layout, title = 'Get Data')	
+# 	return Panel(child = o_layout, title = 'Get Data')	
 
 # ======================================================================
 # WRAP UP
@@ -97,7 +99,7 @@ def create_tab_four():
 o_tab1 = create_tab_one()
 o_tab2 = create_tab_two()
 o_tab3 = create_tab_three()
-#o_tab4 = create_tab_four()
+# o_tab4 = create_tab_four()
 
 o_tabs = Tabs(tabs = [o_tab1, o_tab2, o_tab3])#, o_tab4])
 curdoc().add_root(o_tabs)
